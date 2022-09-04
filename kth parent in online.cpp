629ef3b9381a7b62a862743ed2a9db19fd0f1c68
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2022-09-04 06:23:47 +0600

    Time Complexity: O(V Log V)

--- kth parent in online.cpp
+++ kth parent in online.cpp
@@ -6,39 +6,7 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-#define       ll                  long long int
-#define       veci                 vector<int>
-#define       vecl                 vector<long long int>
-#define       pb                   push_back
-#define       newline cerr << endl
-#define       pii                  pair<int, int>
-#define       F                    first
-#define       S                    second
-#define       nl                   "\n"
-#define       all(v)               v.begin(), v.end()
-///============ CONSTANT ===============///
-#define mx9   1000000007
-#define mx5   200005
-#define inf   1<<30                                           //infinity value
-#define eps   1e-9
-#define mod   mx9
-///=============== Debugging ============================///
-#ifndef ONLINE_JUDGE
-#define       debug(x) cout << #x << " = " << x << endl
-#define       debug2(x, y)             cout << #x << ": " << x << " | " << #y << ": " << y << endl;
-#define       debug3(x, y, z)          cout << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
-#define       debug4(a, b, c, d)       cout << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
-#endif
-void init_code()
-{
-    ios::sync_with_stdio(false); cin.tie(0);cout.tie(0);
-    
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("Error.txt", "w", stderr);
-    #endif    
-}
+#define pb push_back
 const int N = 1e5+5;
 int par[N][20];
 vector<int> g[N];
@@ -60,7 +28,6 @@ int kth_parent(int u, int k)
 }
 int main()
 {
-    init_code();
     int n;
     cin >> n;
     for(int i=0; i<n; i++)
@@ -89,7 +56,7 @@ int main()
         int node, k; // kth parent of node
         cin >> node >> k;
         int ans = kth_parent(node, k);
-        cout << ans << nl;
+        cout << ans << '\n';
     }
      
 return 0;
