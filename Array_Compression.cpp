f1bc2470028df7ce995fdd0fa6316b11d94e7978
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2022-09-05 13:30:30 +0600

    Create Array_Compression.cpp

---
+++ Array_Compression.cpp
@@ -0,0 +1,22 @@
+const int N = 2e5+5;
+int L[N], R[N];
+void array_compression()
+{
+    set<int> Set;
+    for(int i=1; i<=n; i++)
+    {
+        cin >> L[i] >> R[i];
+        Set.insert(L[i]);
+        Set.insert(R[i]);
+        Set.insert(R[i]+1);
+    }
+    map<int, int> Map, key;
+    int idx = 0;
+    for(auto x: Set) Map[x] = ++idx;
+    for(int i=1; i<=n; i++)
+    {
+        L[i] = Map[L[i]];
+        R[i] = Map[R[i]];
+    }
+}
+// Total distinct number will be at most 8*10^5
