83521ee3b19072505ec1c81f92fea2f0aa3e6843
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2022-10-19 01:29:17 +0600

    Time-Complexity: O(V*E) [Updated]

--- Bellman-Ford.cpp
+++ Bellman-Ford.cpp
@@ -22,66 +22,70 @@ const int N = 1e3+3;
 int v, e, dist[N];
 struct Edge
 {
-	int x, y, weight;
-}E[N];
+    int x, y, weight;
+};
+vector<Edge> E;
 
 inline int BellmanFord(int source)
 {
-	for(int i=1; i<=v; i++)
-	{
-		if(i == source) dist[i] = 0;
-		else dist[i] = inf;
-	}
-	bool done = false;
-	for(int i=0; i<v and !done; i++)
-	{
-		done = true;
-		for(int j=0; j<e; j++)
-		{
-			int so = E[j].x;
-			int de = E[j].y;
-			
-			if(dist[so] + E[j].weight < dist[de])
-			{
-				dist[de] = dist[so] + E[j].weight;
-				done = false;
-			}
-		}
-	}
-	if(!done) return -1;
-	else return 0;
+    for(int i=0; i<v; i++)
+    {
+        if(i == source) dist[i] = 0;
+        else dist[i] = inf;
+    }
+    bool done = false;
+    for(int i=0; i<v and !done; i++)
+    {
+        done = true;
+        for(int j=0; j<e; j++)
+        {
+            int so = E[j].x;
+            int de = E[j].y;
+            
+            if(dist[so] + E[j].weight < dist[de])
+            {
+                dist[de] = dist[so] + E[j].weight;
+                done = false;
+            }
+        }
+    }
+    if(!done) return -1;
+    else return 0;
 }
-// void init_code()
-// {
-//     ios::sync_with_stdio(false); cin.tie(0);cout.tie(0);
+void init_code()
+{
+    ios::sync_with_stdio(false); cin.tie(0);cout.tie(0);
     
-//     #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-//     freopen("Error.txt", "w", stderr);
-//     #endif    
-// }
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("Error.txt", "w", stderr);
+    #endif    
+}
 int main()
 {
-	//init_code();
-	int x, y, z;
-	scanf("%d %d", &v, &e);
-	for(int i=0; i<e; i++)
-	{
-		scanf("%d %d %d", &x, &y, &z);
-		E[i].x = x;
-		E[i].y = y;
-		E[i].weight = z;
-	}
-	int src = 1;
-	int is_poosible = BellmanFord(src);
-	
-	if(is_poosible == -1) printf("Detect Negative Cycle\n");
-	else
-	{
-		for(int i=1; i<=v; i++)
-		{
-			printf("Node %d: %d\n", i, dist[i]);
-		}
-	}
+    init_code();
+    int x, y, z;
+    scanf("%d %d", &v, &e);
+    for(int i=0; i<e; i++)
+    {
+        scanf("%d %d %d", &x, &y, &z);
+        Edge get;
+        get.x = x;
+        get.y = y;
+        get.weight = z;
+        E.pb(get);
+    }
+    int src = 0; // 0-based
+    int is_poosible = BellmanFord(src);
+    
+    if(is_poosible == -1) printf("Detect Negative Cycle\n");
+    else
+    {
+        for(int i=0; i<v; i++)
+        {
+            printf("Node %d: %d\n", i, dist[i]);
+        }
+    }
+    E.clear();
 }
