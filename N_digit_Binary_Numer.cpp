66cfb9072bfa63cc424c1bd37abdea1a7c2324b8
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2023-02-20 23:54:14 +0600

    Time Complexity: 2^N

--- N_digit_Binary_Numer.cpp
+++ N_digit_Binary_Numer.cpp
@@ -26,3 +26,36 @@ int main()
     cin >> n;
     Generate(0);
 }
+
+
+/// M digit of 1's on N
+
+int n, m, a[21];
+void print()
+{
+    for(int i=0; i<n; i++)
+    {
+        cout << a[i];
+    }
+    cout << endl;
+}
+void Generate(int i, int cnt)
+{
+    if(i==n)
+    {
+        if(cnt == m) print();
+        return;
+    }
+    a[i] = 0;
+    Generate(i+1, cnt);
+    a[i] = 1;
+    Generate(i+1, cnt+1);
+}
+int main()
+{
+    init_code();
+    cin >> n >> m;
+    Generate(0, 0);
+     
+return 0;
+}
