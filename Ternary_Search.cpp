d4657b1ae1519aedbba52ce504469524b4963dac
Mahabub Rahman <45081033+Mahabub618@users.noreply.github.com>
2023-04-08 18:51:39 +0600

    Time Complexity: O(log3 N)

--- Ternary_Search.cpp
+++ Ternary_Search.cpp
@@ -1,16 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 // finds where f(x) is minimum
-
-int main() {
-
-    double lo = 0, mid = 1e9; // appropriate limits;
-    for(int itr = 0; itr < 100; itr++) {
-        double m1 = (lo+lo+mid)/3.0, m2 = (lo+hi+hi)/3.0;
+/* double f(double x)
+{
+    
+}*/
+int main()
+{
+    double lo = 0, hi = 1e18;
+    for(int itr = 0; itr < 100; itr++)
+    {
+        double m1 = (lo+lo+hi)/3.0, m2 = (lo+hi+hi)/3.0;
         if(f(m1) > f(m2)) lo = m1;
         else hi = m2;
     }
-
-    return 0;
+    cout << f(lo) << endl;
 }
